807520fac557ac95a12f2d87b93a03814df69500
Cezary-Pokropek <cezary.pokropek@gmail.com>
2020-02-16 13:22:29 +0100

    Created Actor(Enemy,Item) Spawn Volume prototype

---
+++ Source/AdventureGame/SpawnVolume.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnVolume.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Engine/World.h"
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawningBox"));
+	if (!ensure(SpawningBox != nullptr)) return;
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Getting a random point in our spawn volume and return that
+FVector ASpawnVolume::GetSpawnPoint()
+{
+	FVector Extent = SpawningBox->GetScaledBoxExtent();
+	FVector Origin = SpawningBox->GetComponentLocation();
+
+	FVector Point = UKismetMathLibrary::RandomPointInBoundingBox(Origin, Extent);
+
+	return Point;
+}
+
+TSubclassOf<AActor> ASpawnVolume::GetSpawnActor()
+{
+	if (SpawnArray.Num() > 0)
+	{
+		int32 Selection = FMath::RandRange(0, SpawnArray.Num() - 1);
+
+		return SpawnArray[Selection];
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+void ASpawnVolume::SpawnOurActor_Implementation(UClass* ToSpawn, const FVector& Location)
+{
+	if (ToSpawn)
+	{
+		UWorld* World = GetWorld();
+		FActorSpawnParameters SpawnParams;
+
+		if (World)
+		{
+			AActor* Actor = World->SpawnActor<AActor>(ToSpawn, Location, FRotator(0.f), SpawnParams);
+		}
+	}
+}
