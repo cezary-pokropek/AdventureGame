a6deb9830c085f489a92af364197b0ef2b197978
Cezary-Pokropek <cezary.pokropek@gmail.com>
2020-02-26 23:17:53 +0100

    Created Item class with Pickup and Explosive derived classes

---
+++ Source/AdventureGame/Pickup.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREGAME_API APickup : public AItem
+{
+	GENERATED_BODY()
+	
+public:
+
+	APickup();
+
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+
+};
